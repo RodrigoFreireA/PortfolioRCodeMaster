4027fa42c507772dcaadc5785dabe0c8c1cfdddf
RodrigoFreireA <rodrigofandradee@gmail.com>
2023-10-19 17:19:03 -0300

    update vendor

--- vendor/laravel/breeze/stubs/inertia-react/resources/js/Pages/Auth/ForgotPassword.jsx
+++ vendor/laravel/breeze/stubs/inertia-react/resources/js/Pages/Auth/ForgotPassword.jsx
@@ -32,19 +32,19 @@ export default function ForgotPassword({ status }) {
 
             <form onSubmit={submit}>
                 <TextInput
-                    id="password"
+                    id="email"
                     type="email"
                     name="email"
                     value={data.email}
                     className="mt-1 block w-full"
                     isFocused={true}
-                    handleChange={onHandleChange}
+                    onChange={onHandleChange}
                 />
 
                 <InputError message={errors.email} className="mt-2" />
 
                 <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" processing={processing}>
+                    <PrimaryButton className="ml-4" disabled={processing}>
                         Email Password Reset Link
                     </PrimaryButton>
                 </div>
